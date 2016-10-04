6de6224e37c244296802ade517ae2cb2c4ba82b0
lynetteduran <lynetteduran@Lynettes-MacBook-Air.local>
2016-10-04 15:02:33 -0700

    completes challenges 1 and 2

--- challenge_1/js/app.js
+++ challenge_1/js/app.js
@@ -1,3 +1,10 @@
 console.log('challenge #1 -- app.js loaded!');
 
+var app = angular.module("dayOneExampleApp", []);
+app.controller("WelcomeController", WelcomeController);
 // nothing to see here!
+
+function WelcomeController() {
+	var vm = this;
+	vm.name = "foo";
+}
--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,28 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
+var app = angular.module("appTwo", []);
+app.controller("WelcomeController", WelcomeController);
+app.controller("ClassController", ClassController);
 
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME"
-// }
+function WelcomeController() {
+  var vm = this;
+  vm.full_name = "Lynette Durán";
+  vm.age = "nobody's biz";
+  vm.city = "All of ";
+  vm.state = "California";
+  vm.name_length = vm.full_name.length;
+  vm.shout = function() {
+  	return vm.full_name.toUpperCase() + "!";
+  }
+}
+
+function ClassController() {
+  var vm = this;
+  vm.class_name = "GAIA";
+  vm.enrolled_students = "12";
+  vm.start_date = "September 6, 2016";
+  vm.end_date = "November 30, 2016";
+  vm.daysRemaining = function() {
+  	return "Many";
+  }
+}
